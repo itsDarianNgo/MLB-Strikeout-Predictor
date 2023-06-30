883849e954398b993f0e9d2867ea8a353c9ecbb7
itsdarianngo <darianngo@gmail.com>
2023-06-30 06:18:45 -0500

    Added feature to calculate avg strikeouts per year

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -2,22 +2,37 @@ import pandas as pd
 from data_preprocessing import load_data, clean_data, merge_data, save_final_data
 
 
-def generate_avg_strikeouts_per_year_feature(data):
-    """Generates the average strikeouts per year feature for each player."""
-    data["Year"] = pd.DatetimeIndex(data["Date"]).year
-    avg_so_per_year = data.groupby(["Player", "Year"])["SO_y"].mean().reset_index()
-    avg_so_per_year.rename(columns={"SO_y": "avg_strikeouts_per_year"}, inplace=True)
-    return avg_so_per_year
+def calculate_cumulative_average_strikeouts(player_data):
+    """Calculate the cumulative average strikeouts per year for a single player."""
+    # Extract year from date and calculate running sum and count per year
+    player_data["Year"] = pd.DatetimeIndex(player_data["Date"]).year
+    player_data["SO_y_shifted"] = player_data.groupby("Year")["SO_y"].shift()
+    player_data["Cumulative_SO"] = player_data.groupby("Year")["SO_y_shifted"].cumsum()
+    player_data["Game_Count"] = player_data.groupby("Year").cumcount()
+
+    # Calculate cumulative average strikeouts, with the first game set to 0
+    player_data["Avg_SO_per_year"] = 0
+    player_data.loc[player_data["Game_Count"] != 0, "Avg_SO_per_year"] = player_data["Cumulative_SO"] / player_data["Game_Count"]
+
+    # Drop auxiliary columns
+    player_data = player_data.drop(columns=["SO_y_shifted", "Cumulative_SO", "Game_Count", "Year"])
+
+    return player_data
 
 
-def generate_features(data, feature_generators):
-    """Generates features for the data using a list of feature generator functions."""
-    # Sort the data by player and date
-    data.sort_values(["Player", "Date"], inplace=True)
+def generate_features(data):
+    """Generate features for the dataset."""
 
-    features = [feature_generator(data) for feature_generator in feature_generators]
-    feature_df = pd.concat(features, axis=1)
-    return feature_df
+    players = data["Player"].unique()
+    player_dfs = [calculate_cumulative_average_strikeouts(data[data["Player"] == player].copy()) for player in players]
+
+    # Combine all player data
+    historical_df = pd.concat(player_dfs, axis=0)
+
+    # Drop rows with missing values
+    historical_df.dropna(axis=0, how="any", inplace=True)
+
+    return historical_df
 
 
 def main():
@@ -26,25 +41,8 @@ def main():
     batting_data, pitching_data, game_data = clean_data(batting_data, pitching_data, game_data)
     final_data = merge_data(batting_data, pitching_data, game_data)
 
-    # Specify the feature generator functions
-    feature_generators = [generate_avg_strikeouts_per_year_feature]
-
     # Generate the features
-    features_data = generate_features(final_data, feature_generators)
-
-    # Merge the original data with the generated features
-    final_data = pd.merge(final_data, features_data, on=["Player", "Year"], how="left")
-
-    # Sort the data by player and date
-    final_data.sort_values(["Player", "Date"], inplace=True)
-
-    # Define the order of columns
-    first_cols = ["GameID", "Date", "Team", "SO_y"]
-    new_cols = features_data.columns.tolist()  # new feature column names
-    last_cols = [col for col in final_data.columns if col not in first_cols + new_cols]
-
-    # Reorder the columns
-    final_data = final_data[first_cols + new_cols + last_cols]
+    final_data = generate_features(final_data)
 
     # Save the final data
     save_final_data(final_data)
