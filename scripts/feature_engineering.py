47f5c4405c739636eca194dd8c698ecf75cf9ad1
itsdarianngo <darianngo@gmail.com>
2023-07-03 00:31:48 -0500

    Added lagged features

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,6 +1,53 @@
 import pandas as pd
 from data_preprocessing import load_data, clean_data, merge_data, save_final_data
 
+LAG_COLUMNS = [
+    "SO_y",
+    "AB",
+    "R_x",
+    "H_x",
+    "RBI",
+    "BB_x",
+    "SO_x",
+    "PA",
+    "BA",
+    "OBP",
+    "SLG",
+    "OPS",
+    "Pit_x",
+    "Str_x",
+    "WPA_x",
+    "WPA+",
+    "WPA-",
+    "cWPA_x",
+    "acLI_x",
+    "PO",
+    "A",
+    "IP",
+    "H_y",
+    "R_y",
+    "ER",
+    "BB_y",
+    "HR",
+    "ERA",
+    "BF",
+    "Pit_y",
+    "Str_y",
+    "Ctct",
+    "StS",
+    "StL",
+    "GB",
+    "FB",
+    "LD",
+    "Unk",
+    "GSc",
+    "IR",
+    "IS",
+    "WPA_y",
+    "cWPA_y",
+    "acLI_y",
+]
+
 
 def calculate_cumulative_average_strikeouts(player_data):
     """Calculate the cumulative average strikeouts per year for a single player."""
@@ -20,11 +67,34 @@ def calculate_cumulative_average_strikeouts(player_data):
     return player_data
 
 
+def generate_lagged_features(player_data):
+    """Generate lagged features for a single player."""
+    player_data.sort_values(by="Date", inplace=True)
+    for column in LAG_COLUMNS:
+        player_data[f"{column}_lag"] = player_data[column].shift()
+
+    # Define columns to drop (all lag columns except target)
+    columns_to_drop = [col for col in LAG_COLUMNS if col != "SO_y"]
+
+    # Drop original columns excluding the target
+    player_data.drop(columns=columns_to_drop, inplace=True)
+
+    # Fill NaN values with 0
+    player_data.fillna(0, inplace=True)
+
+    return player_data
+
+
 def generate_features(data):
     """Generate features for the dataset."""
-
     players = data["Player"].unique()
-    player_dfs = [calculate_cumulative_average_strikeouts(data[data["Player"] == player].copy()) for player in players]
+
+    player_dfs = []
+    for player in players:
+        player_data = data[data["Player"] == player].copy()
+        player_data = generate_lagged_features(player_data)
+        player_data = calculate_cumulative_average_strikeouts(player_data)
+        player_dfs.append(player_data)
 
     # Combine all player data
     historical_df = pd.concat(player_dfs, axis=0)
@@ -41,11 +111,21 @@ def generate_features(data):
     return historical_df
 
 
+def drop_unnecessary_columns(data):
+    """Drop unnecessary columns from the DataFrame."""
+    columns_to_drop = ["home_score", "away_score", "winner", "venue"]
+    data.drop(columns=columns_to_drop, inplace=True)
+    return data
+
+
 def main():
     # Load, clean, and merge the data
     batting_data, pitching_data, game_data = load_data()
     batting_data, pitching_data, game_data = clean_data(batting_data, pitching_data, game_data)
     final_data = merge_data(batting_data, pitching_data, game_data)
+
+    # Drop unnecessary columns
+    final_data = drop_unnecessary_columns(final_data)
 
     # Generate the features
     final_data = generate_features(final_data)
