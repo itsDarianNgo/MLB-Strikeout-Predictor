4a32751b4b6c9ed6692e045f688f7b55246a0133
itsdarianngo <darianngo@gmail.com>
2023-07-05 06:19:37 -0500

    Working model of predicted odds

--- scripts/calculate_odds.py
+++ scripts/calculate_odds.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 
@@ -15,9 +16,11 @@ def calculate_odds(prediction, betting_line):
     return over_odds, under_odds
 
 
-# Function to load data
-def load_data(filename):
-    return pd.read_csv(filename)
+# Function to load and preprocess data
+def load_and_preprocess_data(filename, date_col):
+    df = pd.read_csv(filename)
+    df[date_col] = pd.to_datetime(df[date_col]).dt.date  # convert to date format and remove time
+    return df
 
 
 # Function to save data
@@ -27,30 +30,37 @@ def save_data(df, filename):
 
 # Main function
 def main():
-    # Load data
-    predictions = load_data("h2o_prediction.csv")
-    betting_lines = load_data("betting_lines.csv")
+    # Get script directory
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Define the directories where the data files are located
+    data_dir = os.path.join(script_dir, "../data/")
+
+    # Load and preprocess data
+    predictions = load_and_preprocess_data(os.path.join(data_dir, "predictions.csv"), "Date")
+    betting_lines = load_and_preprocess_data(os.path.join(data_dir, "prop_odds_history.csv"), "Date")
 
     # Merge data on player name and game date
-    data = pd.merge(predictions, betting_lines, on=["Player", "GameDate"])
+    data = pd.merge(predictions, betting_lines, on=["Player", "Date"])
 
     # Calculate odds
-    data["OverOdds"], data["UnderOdds"] = zip(*data.apply(lambda row: calculate_odds(row["PredictedStrikeouts"], row["BettingLine"]), axis=1))
+    data["OverOdds"], data["UnderOdds"] = zip(*data.apply(lambda row: calculate_odds(row["predictedStrikeouts"], row["prop"]), axis=1))
 
     # Determine whether to bet on over or under
-    data["Bet"] = data.apply(lambda row: "Over" if row["OverOdds"] > row["UnderOdds"] else "Under", axis=1)
+    data["Bet"] = data.apply(lambda row: "Over" if row["predictedStrikeouts"] > row["prop"] else "Under", axis=1)
 
     # Check if the bet was successful
     data["SuccessfulBet"] = data.apply(
-        lambda row: "Yes"
-        if (row["Bet"] == "Over" and row["ActualStrikeouts"] > row["BettingLine"])
-        or (row["Bet"] == "Under" and row["ActualStrikeouts"] < row["BettingLine"])
-        else "No",
+        lambda row: "Yes" if (row["Bet"] == "Over" and row["SO_y"] > row["prop"]) or (row["Bet"] == "Under" and row["SO_y"] < row["prop"]) else "No",
         axis=1,
     )
 
     # Save the results
-    save_data(data, "output/predicted_odds.csv")
+    save_data(data, os.path.join(data_dir, "predicted_odds.csv"))
+
+    # Calculate accuracy
+    accuracy = (data["SuccessfulBet"] == "Yes").mean()
+    print(f"Accuracy: {accuracy * 100:.2f}%")
 
 
 # Run the main function
